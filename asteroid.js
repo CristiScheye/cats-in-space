e46468702f513d287e4715b2035754b438629580
Cristi Scheye <cristi.scheye@gmail.com>
2014-06-25 10:31:23 -0700

    fix asteroid starting position and velocity

--- asteroid.js
+++ asteroid.js
@@ -2,15 +2,16 @@
   var Asteroids = root.Asteroids = (root.Asteroids || {});
 
   function randomVelocity () {
-    var dx = Math.random() * 3 - 8;
-    var dy = Math.random() * 2;
+    var dx = (Math.random() * 2 - 1) * 3;
+    var dy = (Math.random() * 2 - 1) * 3;
     return [ dx, dy ];
   }
 
-  var Asteroid = Asteroids.Asteroid = function (pos, vel) {
-    COLOR = 'white';
-    RADIUS = Math.floor(Math.random() * 10) + 5;
-    Asteroids.MovingObject.call(this, pos, vel, RADIUS, COLOR);
+  var Asteroid = Asteroids.Asteroid = function (pos, vel, coords) {
+    this.color = 'white';
+    var radius = Math.random() * 15 + 15;
+    Asteroids.MovingObject.call(this, pos, vel, radius, this.color);
+    this.coords = coords;
   };
 
   Asteroid.inherits(Asteroids.MovingObject);
@@ -27,15 +28,87 @@
       // start on bottom edge
       x = Math.random() * dimX;
       y = dimY;
-    } else {
+    } else if (startLoc < 0.75) {
       // start on right edge
       x = dimX;
       y = Math.random() * dimY;
-    } 
+    } else {
+      // start on left edge
+      x = 0;
+      y = Math.random() * dimY;
+    }
+    pos = [x, y];
+    coords  = [];
+    var pt_angle = 0;
+    var x, y;
+    var radius = Math.random() * 80 + 10;
+    while (pt_angle < 360) {
+      radius += (Math.random() - 0.5) * 20
+      x = pos[0] + Math.cos(Math.PI / 180 * pt_angle) * radius;
+      y = pos[1] + Math.sin(Math.PI / 180 * pt_angle) * radius;
+      coords.push([x, y]);
+      pt_angle += Math.random() * 40 + 10; //add 10 - 60 deg each time
+    }
 
-  return new Asteroid( [x, y], randomVelocity());
+  return new Asteroid(pos, randomVelocity(), coords);
   };
 
+  // Asteroid.prototype.draw = function (ctx) {
+  //   ctx.beginPath();
+  //   ctx.moveTo(this.coords[0][0], this.coords[0][1]);
+
+  //   for(var i = 1; i < this.coords.length; i++) {
+  //     ctx.lineTo(this.coords[i][0], this.coords[i][1]);
+  //   }
+  //   ctx.closePath();
+  //   ctx.strokeStyle = this.color;
+  //   ctx.stroke();
+  // };
+
+  Asteroid.prototype.move = function () {
+    var dx = this.vel[0];
+    var dy = this.vel[1];
+    this.pos = [this.pos[0] + dx, this.pos[1] + dy];
+    this.coords.forEach(function(coord) {
+      coord[0] += dx;
+      coord[1] += dy;
+    })
+  },
+
+
+  Asteroid.prototype.isCollidedWith = function (pt, minDist) {
+    function sqr(x) { return x * x }
+    function dist2(v, w) { return sqr(v[0] - w[0]) + sqr(v[1] - w[1]) }
+    function distToSegmentSquared(p, v, w) {
+      var l2 = dist2(v, w);
+      if (l2 == 0) return dist2(p, v);
+      var t = ((p[0] - v[0]) * (w[0] - v[0]) + (p[1] - v[1]) * (w[1] - v[1])) / l2;
+      if (t < 0) return dist2(p, v);
+      if (t > 1) return dist2(p, w);
+      return dist2(p, [v[0] + t * (w[0] - v[0]), v[1] + t * (w[1] - v[1])]);
+    }
+    
+    function distToSegment(p, v, w) { return Math.sqrt(distToSegmentSquared(p, v, w)); }
+
+    for(var i = 0; i < this.coords.length; i++){
+      var ptA = this.coords[i];
+      var ptB;
+
+      console.log("checking collisions in Asteroid class")
+
+      if (i == this.coords.length - 1) {
+        ptB = this.coords[0];
+      } else {
+        ptB = this.coords[i + 1];
+      }
+      debugger;
+      console.log(distToSegment(pt, ptA, ptB))
+      if (minDist >= distToSegment(pt, ptA, ptB)) {
+        return true;
+      }
+    }
+    return false;
+  }
 
 
 })(this);
